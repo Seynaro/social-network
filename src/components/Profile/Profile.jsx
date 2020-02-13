d89781c73043d0488eb2784c8ba13c7b22cd0088
Khamzat <you@example.comxamzatseynaro@gmail.com>
2020-02-13 11:41:38 +0300

    add Dialogs

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import classes from './Profile.module.css';
 import MyPosts from "./My posts/MyPosts";
 
 const Profile = () => {
-    return <div className={classes.profile}>
+    return <div>
         <div className={classes.background}>
             <img src='https://kogdakotika.net/media/post_images/title_page_m_ChtRYfI.jpg' alt=""></img>
         </div>
