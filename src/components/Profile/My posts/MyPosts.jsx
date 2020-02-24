801384281d1d0f6a806f75cfa31a2ff309fadd5c
Khamzat <xamzatseynaro@gmail.com>
2020-02-24 18:12:48 +0300

    add dialogsData messagesData postsData

--- src/components/Profile/My posts/MyPosts.jsx
+++ src/components/Profile/My posts/MyPosts.jsx
@@ -3,6 +3,12 @@ import classes from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let postsData = [
+        {id:1, message:"Hi, how are you?", likesCount: 2},
+        {id:2, message:"It's my first post", likesCount: 11},
+    ]
+
     return (
         <div className={classes.postsBlock}>
             <h3>My posts</h3>
@@ -15,8 +21,8 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post message="Hi, how are you?"/>
-                <Post message="It's my first post"/>
+                <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
+                <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
             </div>
         </div>
     )
