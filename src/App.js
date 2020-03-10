8e107fffaa8f005a9b7b86563714ad5d00a7cf7b
Khamzat <xamzatseynaro@gmail.com>
2020-03-10 13:03:27 +0300

    add actionCreator

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 
 const App = (props) => {
     return (
             <div className='app-wrapper'>
-                <Header/>
-                <Navbar/>
+                <Header />
+                <Navbar />
                 <div className="app-wrapper-content">
                     <Route path='/dialogs'
                            render={ () => <Dialogs
