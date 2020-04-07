29b85c5331f26bda4571b3b9d72a716fb68a5ad1
Khamzat <xamzatseynaro@gmail.com>
2020-04-07 20:09:34 +0300

    login, auth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import classes from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={classes.header}>
         <img
             src='https://static6.depositphotos.com/1035649/553/v/450/depositphotos_5537406-stock-illustration-sign-friendship-love.jpg'>
         </img>
+
+        <div className={classes.loginBlock}>
+            {props.isAuth ? props.login
+                : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
 }
 
