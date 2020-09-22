bc366b7574d70bbb1b3790fa35ae2eb6962ca040
Khamzat <xamzatseynaro@gmail.com>
2020-09-22 22:01:41 +0300

    add React.memo

--- src/components/Profile/My posts/MyPosts.jsx
+++ src/components/Profile/My posts/MyPosts.jsx
@@ -3,30 +3,27 @@ import Post from "./Post/Post";
 import classes from "./MyPosts.module.css"
 import {AddNewPostFormRedux} from "./AddNewPostForm/AddNewPostForm";
 
-class MyPosts extends React.Component {
+const MyPosts = React.memo(props => {
 
-    shouldComponentUpdate(nextProps, nextState) {
+    /*shouldComponentUpdate(nextProps, nextState) {
         return nextProps != this.props || nextState != this.state;
-    }
+    }*/
 
-    render() {
+    let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
 
-        let postsElements = this.props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
+    let onAddPost = (values) => {
+        props.addPost(values.newPostText);
+    };
 
-        let onAddPost = (values) => {
-            this.props.addPost(values.newPostText);
-        };
-
-        return (
-            <div className={classes.postsBlock}>
-                <h3>My posts</h3>
-                <AddNewPostFormRedux onSubmit={onAddPost}/>
-                <div className={classes.posts}>
-                    {postsElements}
-                </div>
+    return (
+        <div className={classes.postsBlock}>
+            <h3>My posts</h3>
+            <AddNewPostFormRedux onSubmit={onAddPost}/>
+            <div className={classes.posts}>
+                {postsElements}
             </div>
-        )
-    }
-}
+        </div>
+    )
+});
 
 export default MyPosts;
