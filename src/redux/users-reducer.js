fdc5bc808588117a00a3a5f3de12ff8ae714c4ec
Khamzat <you@example.comxamzatseynaro@gmail.com>
2020-09-24 15:40:45 +0300

    refactoring follow/unfollow

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -95,25 +95,25 @@ export const requestUsersTC = (page, pageSize) => {
     }
 };
 
+const followUnfollowFlow = async (dispatch, userId, apiMethod, actionCreator) => {
+    dispatch(toggleFollowingProgress(true, userId))
+    let response = await apiMethod(userId)
+
+    if (response.data.resultCode === 0) {
+        dispatch(actionCreator(userId))
+    }
+    dispatch(toggleFollowingProgress(false, userId));
+}
+
 export const followTC = (userId) => {
     return async (dispatch) => {
-        dispatch(toggleFollowingProgress(true, userId));
-        let response = await usersAPI.follow(userId)
-        if (response.data.resultCode === 0) {
-            dispatch(followSuccess(userId))
-        }
-        dispatch(toggleFollowingProgress(false, userId));
+        followUnfollowFlow(dispatch, userId, usersAPI.follow.bind(usersAPI), followSuccess)
     }
 };
 
 export const unfollowTC = (userId) => {
     return async (dispatch) => {
-        dispatch(toggleFollowingProgress(true, userId));
-        let response = await usersAPI.unfollow(userId)
-        if (response.data.resultCode === 0) {
-            dispatch(unfollowSuccess(userId))
-        }
-        dispatch(toggleFollowingProgress(false, userId));
+        followUnfollowFlow(dispatch, userId, usersAPI.unFollow.bind(usersAPI), unfollowSuccess)
     }
 };
 
