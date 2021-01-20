2d6103359ed18f1ab4b0cf33e0cc60bdc7562141
Khamzat <xamzatseynaro@gmail.com>
2021-01-20 23:55:56 +0300

    transform to ts Post

--- src/components/Profile/My posts/Post/Post.tsx
+++ src/components/Profile/My posts/Post/Post.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import classes from './Post.module.css';
 
-const Post = (props) => {
+type PropsType = {
+    message: string
+    likesCount: number
+}
+
+const Post: React.FC<PropsType> = (props) => {
     return (
             <div className={classes.item}>
                 <img src="https://avatars.mds.yandex.net/get-pdb/1767376/0de9f39a-c664-4469-847a-df2c22b1cc16/s1200" alt=""/>
