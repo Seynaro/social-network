c52d235f5178831559c415ec233350b907b3b95b
Khamzat <xamzatseynaro@gmail.com>
2021-01-22 22:26:32 +0300

    transform to ts redux-store

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -10,7 +10,7 @@ import appReducer from "./app-reducer";
 
 
 
-let reducers = combineReducers({
+let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar:sidebarReducer,
@@ -20,8 +20,11 @@ let reducers = combineReducers({
     app: appReducer,
 });
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+type RootReducerType = typeof rootReducer
+export type AppStateType = ReturnType<RootReducerType>
 
+// @ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 export default store;
