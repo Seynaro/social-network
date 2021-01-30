5e23d9cf1d1c710b8256cd7fc65b5686c110eea8
Khamzat <xamzatseynaro@gmail.com>
2021-01-30 23:27:10 +0300

    full transform to ts Login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Field, reduxForm} from "redux-form";
+import {InjectedFormProps, reduxForm} from "redux-form";
 import {createField, Input} from "../Common/Forms/Forms";
 import {requiredField} from "../../utils/validators/validator";
 import {connect} from "react-redux";
@@ -8,65 +8,70 @@ import {Redirect} from "react-router-dom";
 import style from "./../Common/Forms/Forms.module.css";
 import {AppStateType} from "../../redux/redux-store";
 
-const LoginForm = ({handleSubmit, error, captchaUrl}) => {
-    return (
-    <form onSubmit={handleSubmit}>
-
-        {createField("Email", "email", [requiredField], Input)}
-        {createField("Password", "password", [requiredField], Input, {type:"password"})}
-        {createField(null, "rememberMe", [], Input, {type: "checkbox"}, "rememberMe")}
-
-
-        {captchaUrl && <img src={captchaUrl} />}
-        {captchaUrl && createField("Symbols from image", "captcha", [requiredField], Input, {})}
-
-        { error && <div className={style.formSummaryError}>
-            {error}
-        </div>}
-
-        <div>
-            <button>Login</button>
-        </div>
-    </form>)
-};
-
-const LoginReduxForm = reduxForm({
-    form: 'login'
-})(LoginForm);
-
-type LoginType = {
-    loginTC: () => {}
-    isAuth: boolean
-
+type LoginFormOwnPropsType = {
+	captchaUrl: string | null
 }
 
+const LoginForm: React.FC<InjectedFormProps<LoginFormValuesType, LoginFormOwnPropsType> & LoginFormOwnPropsType> = ({handleSubmit, error, captchaUrl}) => {
+	return (
+		<form onSubmit={handleSubmit}>
+
+			{createField<LoginFormValuesTypeKeys>("Email", "email", [requiredField], Input)}
+			{createField<LoginFormValuesTypeKeys>("Password", "password", [requiredField], Input, {type: "password"})}
+			{createField<LoginFormValuesTypeKeys>(undefined, "rememberMe", [], Input, {type: "checkbox"}, "rememberMe")}
+
+
+			{captchaUrl && <img src={captchaUrl}/>}
+			{captchaUrl && createField<LoginFormValuesTypeKeys>("Symbols from image", "captcha", [requiredField], Input, {})}
+
+			{error && <div className={style.formSummaryError}>
+				{error}
+            </div>}
+
+			<div>
+				<button>Login</button>
+			</div>
+		</form>)
+};
+
+const LoginReduxForm = reduxForm<LoginFormValuesType, LoginFormOwnPropsType>({form: 'login'})(LoginForm);
+
 type MapStatePropsType = {
-    captchaUrl: string | null
-    isAuth: boolean
+	captchaUrl: string | null
+	isAuth: boolean
 }
 
 type MapDispatchPropsType = {
-    loginTC: (email: string, password: string, rememberMe: boolean, captcha: string) => {}
+	loginTC: (email: string, password: string, rememberMe: boolean, captcha: string) => {}
 }
 
+type LoginFormValuesType = {
+	email: string
+	password: string
+	rememberMe: boolean
+	captcha: string
+}
+
+type LoginFormValuesTypeKeys = Extract<keyof LoginFormValuesType, string>
+
 const Login: React.FC<MapStatePropsType & MapDispatchPropsType> = ({loginTC, isAuth, captchaUrl}) => {
-    const onSubmit = (formData) => {
-        loginTC(formData.email, formData.password, formData.rememberMe, formData.captcha)
-    };
+	const onSubmit = (formData: LoginFormValuesType) => {
+		loginTC(formData.email, formData.password, formData.rememberMe, formData.captcha)
+	};
 
-    if(isAuth) {
-        return <Redirect to={"/profile"}/>
-    }
+	if (isAuth) {
+		return <Redirect to={"/profile"}/>
+	}
 
-    return <div>
-        <h1>Login</h1>
-        <LoginReduxForm onSubmit={onSubmit} captchaUrl={captchaUrl}/>
-    </div>
+	return <div>
+		<h1>Login</h1>
+		<LoginReduxForm onSubmit={onSubmit} captchaUrl={captchaUrl}/>
+	</div>
 };
 
 const mapStateToProps = (state: AppStateType): MapStatePropsType => ({
-    captchaUrl: state.auth.captchaUrl,
-    isAuth: state.auth.isAuth
+	captchaUrl: state.auth.captchaUrl,
+	isAuth: state.auth.isAuth
 })
 
 export default connect(mapStateToProps, {loginTC})(Login);
