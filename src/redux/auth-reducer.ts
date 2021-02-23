4557c799fd8e3753177679567914a0b4a5e09c82
Khamzat <xamzatseynaro@gmail.com>
2021-02-23 11:26:26 +0300

    convert to ts auth-reducer

--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,108 +1,80 @@
-import {ResultCodeForCaptchaEnum, ResultCodesEnum} from "../api/api";
+import {ResultCodeForCapcthaEnum, ResultCodesEnum} from "../api/api";
 import {stopSubmit} from "redux-form";
-import {Dispatch} from "redux";
-import { authAPI } from "../api/auth-api";
-import { securityAPI } from "../api/security-api";
+import {authAPI} from '../api/auth-api';
+import {securityAPI} from '../api/security-api';
+import {BaseThunkType, InferActionsTypes} from './redux-store';
+import {Action} from 'redux';
+import {FormAction} from 'redux-form/lib/actions';
 
-const SET_USER_DATA = 'auth/SET_USER_DATA';
-const SET_CAPTCHA_URL_SUCCESS = 'auth/SET_CAPTCHA_URL_SUCCESS';
-
-export type InitialStateType = {
-    userId: number | null
-    email: string | null
-    login: string | null
-    isAuth: boolean
-    captchaUrl: string | null
-}
-
-let initialState: InitialStateType = {
-    userId: null,
-    email: null,
-    login: null,
+let initialState = {
+    userId: null as (number | null),
+    email: null as string | null,
+    login: null as string | null,
     isAuth: false,
-    captchaUrl: null,
+    captchaUrl: null as string | null// if null, then captcha is not required
 };
 
-
-const authReducer = (state = initialState, action: ActionsTypes): InitialStateType => {
+const authReducer = (state = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
-        case SET_USER_DATA:
+        case 'SN/auth/SET_USER_DATA':
+        case 'SN/auth/GET_CAPTCHA_URL_SUCCESS':
             return {
                 ...state,
-                ...action.payload,
-            }
-        case SET_CAPTCHA_URL_SUCCESS:
-            return {
-                ...state,
-                ...action.payload,
+                ...action.payload
             }
         default:
             return state;
     }
-};
-
-type setAuthUserDataActionPayloadType = {
-    userId: number | null
-    email: string | null
-    login: string | null
-    isAuth: boolean
 }
 
-type ActionsTypes = setAuthUserDataActionType | getCaptchaUrlSuccessActionType
-
-type setAuthUserDataActionType = {
-    type: typeof SET_USER_DATA
-    payload: setAuthUserDataActionPayloadType
+export const actions = {
+    setAuthUserData: (userId: number | null, email: string | null, login: string | null, isAuth: boolean) => ({
+        type: 'SN/auth/SET_USER_DATA', payload: {userId, email, login, isAuth}
+    } as const),
+    getCaptchaUrlSuccess: (captchaUrl: string) => ({
+        type: 'SN/auth/GET_CAPTCHA_URL_SUCCESS', payload: {captchaUrl}
+    } as const)
 }
 
-export const setAuthUserData = (userId: number | null, email: string | null, login: string | null, isAuth: boolean): setAuthUserDataActionType => ({
-    type: SET_USER_DATA, payload:
-        {userId, email, login, isAuth}
-});
-
-type getCaptchaUrlSuccessActionType = {
-    type: typeof SET_CAPTCHA_URL_SUCCESS
-    payload: {captchaUrl: string}
-}
-
-export const getCaptchaUrlSuccess = (captchaUrl: string): getCaptchaUrlSuccessActionType => ({
-    type: SET_CAPTCHA_URL_SUCCESS, payload: {captchaUrl}
-});
-
-//type ThunkType = ThunkAction<Promise<void>, AppStateType, unknown, ActionsTypes>
-type DispatchType = Dispatch<ActionsTypes>
-
-export const getAuthUserDataTC = () => async (dispatch: DispatchType) => {
-    let meData = await authAPI.me();
+export const getAuthUserData = (): ThunkType => async (dispatch) => {
+    let meData = await authAPI.me()
     if (meData.resultCode === ResultCodesEnum.Success) {
-        let {id, email, login} = meData.data;
-        dispatch(setAuthUserData(id, email, login, true));
+        let {id, login, email} = meData.data;
+        dispatch(actions.setAuthUserData(id, email, login, true))
     }
-};
+}
 
-export const loginTC = (email: string, password: string, rememberMe: boolean, captcha: string) => async (dispatch: any) => {
-    let loginData = await authAPI.login(email, password, rememberMe, captcha)
-    if (loginData.resultCode === ResultCodesEnum.Success) {
-        dispatch(getAuthUserDataTC());
+export const login = (email: string, password: string, rememberMe: boolean, captcha: string): ThunkType => async (dispatch) => {
+    let data = await authAPI.login(email, password, rememberMe, captcha);
+    if (data.resultCode === ResultCodesEnum.Success) {
+        // success, get auth data
+        dispatch(getAuthUserData())
     } else {
-        if (loginData.resultCode === ResultCodeForCaptchaEnum.CaptchaIsRequired) {
-        dispatch(getCaptchaUrlTC())}
-        let message = loginData.messages.length > 0 ? loginData.messages[0] : "Some error";
-        dispatch(stopSubmit("login", {_error: message}))
-    }
-};
+        if (data.resultCode === ResultCodeForCapcthaEnum.CaptchaIsRequired) {
+            dispatch(getCaptchaUrl());
+        }
 
-export const getCaptchaUrlTC = () => async (dispatch: DispatchType) => {
+        let message = data.messages.length > 0 ? data.messages[0] : "Some error";
+        dispatch(stopSubmit("login", {_error: message}));
+    }
+}
+
+export const getCaptchaUrl = (): ThunkType => async (dispatch) => {
     const data = await securityAPI.getCaptchaUrl()
-    const captchaUrl = data.url;
-    dispatch(getCaptchaUrlSuccess(captchaUrl))
-    };
+    const captchaUrl = data.url
+    dispatch(actions.getCaptchaUrlSuccess(captchaUrl))
+}
 
-export const logoutTC = () => async (dispatch: DispatchType) => {
+export const logout = (): ThunkType => async (dispatch: any) => {
     let response = await authAPI.logout()
-    if (response.data.resultCode === ResultCodesEnum.Success) {
-        dispatch(setAuthUserData(null, null, null, false))
+
+    if (response.data.resultCode === 0) {
+        dispatch(actions.setAuthUserData(null, null, null, false))
     }
-};
+}
 
 export default authReducer;
+
+export type InitialStateType = typeof initialState;
+type ActionsType = InferActionsTypes<typeof actions>
+type ThunkType = BaseThunkType<ActionsType | FormAction>
