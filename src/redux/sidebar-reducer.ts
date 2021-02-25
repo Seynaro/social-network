7b7abb1e23b80a71f2e7b33f4d9ae9f4f6e492fb
Khamzat <xamzatseynaro@gmail.com>
2021-02-25 19:55:24 +0300

    convert to ts sidebar-reducer

--- src/redux/sidebar-reducer.ts
+++ src/redux/sidebar-reducer.ts
@@ -1,7 +1,7 @@
-let initialState = {};
+let initialState = {}
 type InitialStateType = typeof initialState
-const sidebarReducer = (state = initialState, action: any): InitialStateType => {
+const sidebarReducer = (state = initialState, action: any) => {
     return state;
-};
+}
 
 export default sidebarReducer;
