bbcd766f19bc89e794a27b64c7f799888edc4f8b
Khamzat <xamzatseynaro@gmail.com>
2021-02-26 20:11:21 +0300

    convert to ts users-selectors

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,29 +1,31 @@
-import {AppStateType} from "./redux-store";
+import {createSelector} from "reselect";
+import { AppStateType } from "./redux-store";
 
-export const getUsers = (state: AppStateType) => {
-    return state.usersPage.users
+const getUsersSelector = (state: AppStateType) => {
+    return state.usersPage.users;
 }
+
+export const getUsers = createSelector(getUsersSelector,
+    (users) => {
+        return users.filter(u => true);
+    })
 
 export const getPageSize = (state: AppStateType) => {
-    return state.usersPage.pageSize
+    return state.usersPage.pageSize;
 }
 
-export const getTotalUserCount = (state: AppStateType) => {
-    return state.usersPage.totalUserCount
+export const getTotalUsersCount = (state: AppStateType) => {
+    return state.usersPage.totalUsersCount;
 }
 
 export const getCurrentPage = (state: AppStateType) => {
-    return state.usersPage.currentPage
-}
-
-export const getFollowingInProgress = (state: AppStateType) => {
-    return state.usersPage.followingInProgress
+    return state.usersPage.currentPage;
 }
 
 export const getIsFetching = (state: AppStateType) => {
-    return state.usersPage.isFetching
+    return state.usersPage.isFetching;
 }
 
-/*export const get = (state) => {
-return
-}*/
+export const getFollowingInProgress = (state: AppStateType) => {
+    return state.usersPage.followingInProgress;
+}
