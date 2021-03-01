ee6782e894ffde1dc85fa29e709d9177085fcb81
Khamzat <xamzatseynaro@gmail.com>
2021-03-01 20:28:25 +0300

    convert to ts usersContainer

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
+import {connect, DefaultRootState} from 'react-redux';
 import {
-    followTC, requestUsersTC,
-    unfollowTC,
-} from "../../redux/users-reducer";
-import {connect} from "react-redux";
-import Users from "./Users";
-import Preloader from "../Common/Preloader";
+    follow,
+    unfollow, requestUsers
+} from '../../redux/users-reducer';
+import Users from './Users';
+import Preloader from "../Common/Preloader/Preloader";
 import {compose} from "redux";
 import {
     getCurrentPage,
-    getFollowingInProgress, getIsFetching,
+    getFollowingInProgress,
+    getIsFetching,
     getPageSize,
-    getTotalUserCount,
-    getUsers
+    getTotalUsersCount, getUsers
 } from "../../redux/users-selectors";
-import {UserType} from "../../types/types";
-import {AppStateType} from "../../redux/redux-store";
-
-// class UsersContainer extends React.Component<PropsType, StateType>
-// if component dont have local state <PropsType>
-
-// type PropsType = {}
+import { UserType } from '../../types/types';
+import { AppStateType } from '../../redux/redux-store';
 
 type MapStatePropsType = {
     currentPage: number
@@ -31,62 +26,62 @@ type MapStatePropsType = {
     followingInProgress: Array<number>
 }
 
-type OwnPropsType = {}
-
 type MapDispatchPropsType = {
-    requestUsersTC: (currentPage: number, pageSize: number) => void
-    followTC: (userId: number) => void
-    unfollowTC: (userId: number) => void
+    getUsers: (currentPage: number, pageSize: number) => void
+    unfollow: (userId: number) => void
+    follow: (userId: number) => void
 }
 
-type PropsType = MapStatePropsType & MapDispatchPropsType
+type OwnPropsType = {
+    pageTitle: string
+}
+
+type PropsType = MapStatePropsType & MapDispatchPropsType & OwnPropsType
 
 class UsersContainer extends React.Component<PropsType> {
-
     componentDidMount() {
-        const {currentPage, pageSize} = this.props
-        this.props.requestUsersTC(currentPage, pageSize)
-    };
+        const {currentPage, pageSize} = this.props;
+        this.props.getUsers(currentPage, pageSize);
+    }
 
     onPageChanged = (pageNumber: number) => {
-        const {pageSize} = this.props
-        this.props.requestUsersTC(pageNumber, pageSize)
-    };
+        const {pageSize} = this.props;
+        this.props.getUsers(pageNumber, pageSize);
+    }
 
     render() {
-        return <>
-            {this.props.isFetching ? <Preloader/> : null}
 
-            <Users totalUserCount={this.props.totalUsersCount}
+        return <>
+            <h2>{this.props.pageTitle}</h2>
+            {this.props.isFetching ? <Preloader/> : null}
+            <Users totalUsersCount={this.props.totalUsersCount}
                    pageSize={this.props.pageSize}
-                   users={this.props.users}
                    currentPage={this.props.currentPage}
                    onPageChanged={this.onPageChanged}
+                   users={this.props.users}
+                   follow={this.props.follow}
+                   unfollow={this.props.unfollow}
                    followingInProgress={this.props.followingInProgress}
-                   followTC={this.props.followTC}
-                   unfollowTC={this.props.unfollowTC}
             />
         </>
-    };
-
+    }
 }
 
 let mapStateToProps = (state: AppStateType): MapStatePropsType => {
     return {
         users: getUsers(state),
         pageSize: getPageSize(state),
-        totalUsersCount: getTotalUserCount(state),
+        totalUsersCount: getTotalUsersCount(state),
         currentPage: getCurrentPage(state),
-        followingInProgress: getFollowingInProgress(state),
-        isFetching: getIsFetching(state)
+        isFetching: getIsFetching(state),
+        followingInProgress: getFollowingInProgress(state)
     }
-};
+}
+
 
 export default compose(
+    // TStateProps = {}, TDispatchProps = {}, TOwnProps = {}, State = DefaultRootState
     connect<MapStatePropsType, MapDispatchPropsType, OwnPropsType, AppStateType>(
         mapStateToProps,
-        {
-            requestUsersTC, followTC,
-            unfollowTC,
-        })
-    )(UsersContainer);
+        {follow, unfollow, getUsers: requestUsers})
+)(UsersContainer)
