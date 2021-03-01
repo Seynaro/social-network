2c0eb98cee41c7cbbc51dfa18ad8593950cfbc1c
Khamzat <xamzatseynaro@gmail.com>
2021-03-01 20:28:03 +0300

    convert to ts user

--- src/components/Users/User.tsx
+++ src/components/Users/User.tsx
@@ -2,55 +2,47 @@ import React from 'react';
 import styles from "./Users.module.css";
 import userPhoto from "../../assets/images/user.jpg";
 import {NavLink} from "react-router-dom";
-import {UserType} from "../../types/types";
+import {UserType} from '../../types/types';
 
-type OneUserType = {
+type PropsType = {
+    user: UserType
     followingInProgress: Array<number>
-    followTC: (userId: number) => void
-    unfollowTC: (userId: number) => void
-    u: UserType
+    unfollow: (userId: number) => void
+    follow: (userId: number) => void
 }
 
-let User: React.FC<OneUserType> = ({u, followingInProgress, followTC, unfollowTC}) => {
+let User: React.FC<PropsType> = ({user, followingInProgress, unfollow, follow}) => {
     return (
         <div>
+                <span>
+                    <div>
+                       <NavLink to={'/profile/' + user.id}>
+                        <img src={user.photos.small != null ? user.photos.small : userPhoto}
+                             className={styles.userPhoto}/>
+                       </NavLink>
+                    </div>
+                    <div>
+                        {user.followed
+                            ? <button disabled={followingInProgress
+                                .some(id => id === user.id)}
+                                      onClick={() => { unfollow(user.id) }}>
+                                Unfollow</button>
+                            : <button disabled={followingInProgress.some(id => id === user.id)}
+                                      onClick={() => { follow(user.id) }}>
+                                Follow</button>}
+
+                    </div>
+                </span>
             <span>
-               <div>
-                   <NavLink to={'/profile/' + u.id}>
-                 <img src={u.photos.small != null ? u.photos.small : userPhoto}
-                      className={styles.userPhoto}/>
-               </NavLink>
-               </div>
-               <div>
-                   {u.followed
-                       ? <button disabled={followingInProgress
-                           .some(id => id === u.id)}
-                                 onClick={() => {
-                                     unfollowTC(u.id)
-                                 }
-                                 }>Unfollow</button>
-
-                       : <button disabled={followingInProgress
-                           .some(id => id === u.id)}
-                                 onClick={() => {
-                                     followTC(u.id)
-                                 }
-                                 }>Follow</button>}
-               </div>
-            </span>
-            <span>
-
-               <span>
-                 <div>{u.name}</div>
-                 <div>{u.status}</div>
-               </span>
-
-               <span>
-                 <div>{"u.location.country"}</div>
-                 <div>{"u.location.city"}</div>
-               </span>
-
-            </span>
+                    <span>
+                        <div>{user.name}</div>
+                        <div>{user.status}</div>
+                    </span>
+                    <span>
+                        <div>{"user.location.country"}</div>
+                        <div>{"user.location.city"}</div>
+                    </span>
+                </span>
         </div>)
 }
 
