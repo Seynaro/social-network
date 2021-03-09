62325967e6183c6fade486500bf7b838c0ba6572
Khamzat <xamzatseynaro@gmail.com>
2021-03-09 23:28:41 +0300

    convert to ts redux-store.ts

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,33 +1,36 @@
-import {applyMiddleware, combineReducers, compose, createStore} from "redux";
+import {Action, applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
-import thunkMiddleware from "redux-thunk";
-import {reducer as reducerForm} from "redux-form"
+import thunkMiddleware, {ThunkAction} from "redux-thunk";
+import {reducer as formReducer} from 'redux-form'
 import appReducer from "./app-reducer";
-
-
 
 let rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
-    sidebar:sidebarReducer,
+    sidebar: sidebarReducer,
     usersPage: usersReducer,
     auth: authReducer,
-    form: reducerForm,
-    app: appReducer,
-});
+    form: formReducer,
+    app: appReducer
+})
 
-type RootReducerType = typeof rootReducer
+type RootReducerType = typeof rootReducer; // (globalstate: AppStateType) => AppStateType
 export type AppStateType = ReturnType<RootReducerType>
 
-type PropertiesTypes<T> = T extends {[key: string]: infer U} ? U : never
-export type InferActionsTypes<T extends {[key: string]: (...args: any[]) => any}> = ReturnType<PropertiesTypes<T>>
+export type InferActionsTypes<T> = T extends { [keys: string]: (...args: any[]) => infer U } ? U : never
+
+export type BaseThunkType<A extends Action = Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, A>
+
 
 // @ts-ignore
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-export default store;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)))
+// @ts-ignore
+window.__store__ = store
+
+export default store
