29a43c6b3d726f5a783d344b947e228e9018495e
Khamzat <xamzatseynaro@gmail.com>
2021-08-17 14:36:42 +0300

    convert to ts DialogsContainer.tsx

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -1,26 +1,18 @@
-import React from "react";
-import {sendMessageCreator} from "../../redux/dialogs-reducer";
-import Dialogs from "./Dialogs";
-import {connect} from "react-redux";
-import {withAuthRedirect} from "../../hoc/withAuthRedirect";
-import {compose} from "redux";
+import React from 'react';
+import {actions} from '../../redux/dialogs-reducer';
+import Dialogs from './Dialogs';
+import {connect} from 'react-redux';
+import {withAuthRedirect} from '../../hoc/withAuthRedirect';
+import {compose} from 'redux';
+import {AppStateType} from '../../redux/redux-store';
 
-
-const mapStateToProps = (state) => {
+let mapStateToProps = (state: AppStateType) => {
     return {
-        dialogsPage: state.dialogsPage,
+        dialogsPage: state.dialogsPage
     }
-};
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        sendMessage: (newMessageBody) => {
-            dispatch(sendMessageCreator(newMessageBody));
-        }
 }
-};
 
-export default compose(
-    connect(mapStateToProps, mapDispatchToProps),
+export default compose<React.ComponentType>(
+    connect(mapStateToProps, {...actions}),
     withAuthRedirect
-)(Dialogs);
+)(Dialogs)
